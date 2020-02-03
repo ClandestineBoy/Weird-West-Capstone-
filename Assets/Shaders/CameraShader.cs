98109881bde037d1f5f9c6efc6b3e1144ebf2439
kevin_ray <kray2017@gmail.com>
2020-02-03 14:53:10 -0500

    Amplify, Darkness, Headbobber

---
+++ Assets/Shaders/CameraShader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraShader : MonoBehaviour
+{
+    public Material mat;
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().farClipPlane = 50;
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+    private void OnDisable()
+    {
+        GetComponent<Camera>().farClipPlane = 1000;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
+
